a3d42b5f1e3ee1ab5485074adfc76c361c5aeb74
4ndr <andrepereiraleite@gmail.com>
2019-09-15 21:46:53 -0300

    Udemy Course
    Signed-off-by: 4ndr <andrepereiraleite@gmail.com>

---
+++ backend/db.js
@@ -0,0 +1,32 @@
+const mongodb = require('mongodb');
+
+const MongoClient = mongodb.MongoClient;
+const mongoDbUrl = 
+    'mongodb+srv://udemy_course:udemy_course@cluster0-x724e.mongodb.net/test?retryWrites=shop&w=majority';
+
+let _db;
+
+const initDb = callback => {
+    if (_db){
+        console.log("Database is already initialized");
+        return callback(null, _db);
+    }
+    MongoClient.connect(mongoDbUrl).then(client => {
+        _db = client;
+        callback(null, _db);
+    }).catch(err => {
+        callback(err);
+    })
+}
+
+const getDb = () => {
+    if (!_db){
+        throw Error('Database not initialized');
+    }
+    return _db;
+}
+
+module.exports = {
+    initDb,
+    getDb
+};
